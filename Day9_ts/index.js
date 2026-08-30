268368a91e61c29c09255c7a7ce16c2a9b5e93e0
Sammit Bhoi <bhoisammit63@gmail.com>
2026-08-30 18:17:52 +0530

    Basic typescript and tsc

---
+++ Day9_ts/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let name1 = "sammit";
+let age = 22;
+console.log(name1);
+console.log(age);
---
+++ Day9_ts/index.ts
@@ -0,0 +1,5 @@
+let name1:string="sammit";
+let age:number=22;
+console.log(name1);
+console.log(age);
+
