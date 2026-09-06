52b352f1baa3778eec8e9b1971976ece544ff88a
Sammit Bhoi <bhoisammit63@gmail.com>
2026-09-07 00:34:56 +0530

    study basic function

---
+++ Day12/function.js
@@ -0,0 +1,6 @@
+"use strict";
+hello("ram"); // hoisting of function
+function hello(msg) {
+    console.log(msg);
+}
+hello('sammit'); //argument
---
+++ Day12/function.ts
@@ -0,0 +1,9 @@
+
+hello("ram") // hoisting of function
+
+function hello(msg:string)//parameter  // function declaration is hoisted i.e we can invoke before declaration
+{
+    console.log(msg)
+}
+
+hello('sammit')  //argument
